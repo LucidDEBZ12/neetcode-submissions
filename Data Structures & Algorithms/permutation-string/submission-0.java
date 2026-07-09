2b79992d7367e44abfa1c552803c68497103b6bf
Debansu <138821409+LucidDEBZ12@users.noreply.github.com>
2026-07-09 11:48:23 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] s1Count = new int[26];
+        int[] windowCount = new int[26];
+
+        // Build frequency arrays
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            windowCount[s2.charAt(i) - 'a']++;
+        }
+
+        // Compare first window
+        if (Arrays.equals(s1Count, windowCount)) {
+            return true;
+        }
+
+        // Slide the window
+        for (int i = s1.length(); i < s2.length(); i++) {
+
+            // Add new character
+            windowCount[s2.charAt(i) - 'a']++;
+
+            // Remove leftmost character
+            windowCount[s2.charAt(i - s1.length()) - 'a']--;
+
+            // Compare frequencies
+            if (Arrays.equals(s1Count, windowCount)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
